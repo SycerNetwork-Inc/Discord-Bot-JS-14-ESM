3bdcf17baa9cbfed11184f0a6a48067ff070289a
ⵝⵉⵏⵏⴻⵔⴿⵓⵏ <45807795+TinnerKun@users.noreply.github.com>
2022-10-03 23:18:20 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const token = "";
+export const lavalink = [
+    {
+        host: '192.168.0.xxx',
+        port: 2333,
+        password: 'youshallnotpass'
+    }
+]
+
+export const clientId = '738445622792945756'; // bot id
