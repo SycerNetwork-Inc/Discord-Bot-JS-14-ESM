dd3edbb2fdac85ec668c0ef915792cd59c9f1608
ⵝⵉⵏⵏⴻⵔⴿⵓⵏ <45807795+TinnerKun@users.noreply.github.com>
2023-01-05 03:31:21 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -7,6 +7,7 @@ export async function execute(oldState, newState, client) {
     const player = client.manager.players.get(newState.guild.id);
     if(player) {
         const voiceChannel = client.channels.cache.get(player.voiceChannel);
+        if(!voiceChannel) return player.destroy();
         if(voiceChannel.members.size === 1) {
             console.log('No one is in the voice channel, destroying player.');
             player.destroy();
